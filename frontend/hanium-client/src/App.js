c1a8fbd2d9e0b23f84672ded885f9169718c211b
박현철 <hyunchul1998@naver.com>
2022-07-23 00:25:53 +0900

    Connect React with Spring-boot

--- frontend/hanium-client/src/App.js
+++ frontend/hanium-client/src/App.js
@@ -3,16 +3,15 @@ import axios from 'axios';
 
 function App() {
    const [hello, setHello] = useState('')
-
     useEffect(() => {
-        axios.get('/api/hello')
+        axios.get('/api/')
         .then(response => setHello(response.data))
         .catch(error => console.log(error))
     }, []);
 
     return (
         <div>
-            백엔드에서 가져온 데이터입니다 : {hello}
+            백엔드에서 가져온 데이터입니다. : {hello}
         </div>
     );
 }
