bc4ad6c42997f7bc413a02933378daf010f7b90c
kksks3192@gmail.com <kksks3192@gmail.com>
2022-07-28 21:12:52 +0900

    add websocket chat function

--- src/main/java/com/hanium/cloudproject/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.hanium.cloudproject.repository;
-
-
-import com.hanium.cloudproject.entity.Product;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductRepository extends CrudRepository<Product/*관리대상 Entity*/, Long/*대표값id 타입*/> {
-}
