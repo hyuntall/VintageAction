176ca09a7aca3e2c712b700a842905d4256a8910
박현철 <hyunchul1998@naver.com>
2022-08-09 17:46:40 +0900

    Modify Vintage.js and Vintage.css

--- frontend/hanium-client/src/components/VintageUploadForm.js
+++ frontend/hanium-client/src/components/VintageUploadForm.js
@@ -1,14 +1,16 @@
-import { React, useState} from "react";
+import { React, useState, useRef} from "react";
 import axios from 'axios';
 import { useNavigate } from "react-router-dom";
-//import '../auth.css'
 
 const VintageUploadForm =({memberObj, refreshMember}) => {
     const [title, setTitle] = useState("");
     const [detail, setDetail] = useState("");
     const [itemName, setItemName] = useState("");
     const [price, setPrice] = useState(0);
+    const [attachment, setAttachment] = useState(null);
     const [error, setError] = useState(null)
+
+    const fileInput = useRef();
     //const [memberObj, setMemberObj] = useState(null);
     const onChange = (event) => {
         const {target: {name, value}} = event;
@@ -22,6 +24,19 @@ const VintageUploadForm =({memberObj, refreshMember}) => {
             setPrice(value)
         }
     }
+
+    const onFileChange = (event) => {
+        // 이미지 입력 시 url을 읽는 함수
+        const {target:{files}} = event;
+        const theFile = files[0];
+        const reader = new FileReader();
+        reader.onloadend = (finishEvent) => {
+            const {currentTarget: { result }} = finishEvent
+            setAttachment(result)
+        }
+        reader.readAsDataURL(theFile);
+    }
+
     const onSubmit = async (event) => {
         
         event.preventDefault();
@@ -41,42 +56,75 @@ const VintageUploadForm =({memberObj, refreshMember}) => {
     };
     return (
         <>
-            <form onSubmit={onSubmit} className="container">
-                <input 
-                name="title"
-                placeholder="title" 
-                required 
-                value={title}
-                onChange={onChange}
-                className="authInput"/>
-
-                <input 
-                name="detail"
-                placeholder="detail" 
-                required 
-                value={detail}
-                onChange={onChange}
-                className="authInput"/>
-
-                <input 
-                name="item-name"
-                placeholder="item name" 
-                required 
-                value={itemName}
-                onChange={onChange}
-                className="authInput"/>
-
-                <input 
-                name="price"
-                placeholder="price" 
-                required 
-                value={price}
-                onChange={onChange}
-                className="authInput"/>
-
+            <form onSubmit={onSubmit} className="vintage-upload-form">
+                <div className="image-form">
+                    <img className="item-image"
+                    src={attachment ? (attachment):(require("../img/temp.png"))}/>
+                    <br/>
+                    <label htmlFor="item-image">이미지 등록</label>
+                    <input 
+                    type="file"
+                    name="item-image"
+                    required
+                    id="item-image"
+                    onChange={onFileChange}
+                    ref={fileInput}
+                    />
+                </div>
+                <div className="text-form">
+                    <label htmlFor="title-input">제목</label>
+                    <input 
+                    name="title"
+                    placeholder="title" 
+                    required 
+                    value={title}
+                    onChange={onChange}
+                    className="title-input"
+                    id="title-input"/>
+                    <br/>
+                    <label htmlFor="item-name-input">상품명</label>
+                    <input 
+                    name="item-name"
+                    placeholder="item name" 
+                    required 
+                    value={itemName}
+                    onChange={onChange}
+                    className="item-name-input"
+                    id="item-name-input"/>
+                    <br/>
+                    <label htmlFor="price-input">가격</label>
+                    <input 
+                    name="price"
+                    placeholder="price" 
+                    required 
+                    value={price}
+                    onChange={onChange}
+                    className="price-input"
+                    id="price-input"/>
+                    <br/>
+                    <label htmlFor="detail-input">설명</label>
+                    <input 
+                    name="detail"
+                    placeholder="detail" 
+                    required 
+                    value={detail}
+                    onChange={onChange}
+                    className="detail-input"
+                    id="detail-input"/>
+                    <br/>
+                    <label htmlFor="category">카테고리</label>
+                    <select name="category" id="category">
+                        <option value="clothes">의류</option>
+                        <option value="test1">test1</option>
+                        <option value="test2">test2</option>
+                        <option value="test3">test3</option>
+                        <option value="test4">test4</option>
+                        <option value="test5">test5</option>
+                    </select>
+                </div>
                 <input type="submit" 
-                className="authInput authSubmit"
-                value="Upload"/>
+                className="vintage-submit"
+                value="등록"/>
                 {error && <span className="authError">{error}</span>}
             </form>
         </>
