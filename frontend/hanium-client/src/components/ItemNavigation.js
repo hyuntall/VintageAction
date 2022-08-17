72e9e5709854f66f53e438c511a551c0ee415fd9
6loss0m <gsjang0807@gmail.com>
2022-08-17 21:57:53 +0900

    icon add, ItemNavigation modify

--- frontend/hanium-client/src/components/ItemNavigation.js
+++ frontend/hanium-client/src/components/ItemNavigation.js
@@ -1,22 +1,28 @@
 import React from "react";
-import {Link} from "react-router-dom"
-import "../css/Navigation.css"
+import { Link } from "react-router-dom";
+import "../css/Navigation.css";
 const ItemNavigation = () => (
-    <div className="item-navigater-container">
-        <ul className="item-navigator">
-            <li>
-                <Link to="/vintage" className="vintage">
-                    일반 거래
-                </Link>
-            </li>
-            <img className="icon" src={require("../img/logo.png")} />
-            <li>
-                <Link to="/aa" className="뭐였지">
-                    경매 거래
-                </Link>
-            </li>
-        </ul>
-    </div>
-)
+  <div className="item-navigater-container">
+    <ul className="item-navigator">
+      <li>
+        <img className="icon1" src={require("../img/icon1.png")} />
+        <Link to="/vintage" className="vintage">
+          일반 거래
+        </Link>
+      </li>
+      <div className="icon-box">
+        <Link to="/aa">
+          <img className="icon" src={require("../img/logo.png")} />
+        </Link>
+      </div>
+      <li>
+        <Link to="/aa" className="Auction">
+          경매 거래
+        </Link>
+        <img className="icon2" src={require("../img/icon2.png")} />
+      </li>
+    </ul>
+  </div>
+);
 
-export default ItemNavigation
+export default ItemNavigation;
