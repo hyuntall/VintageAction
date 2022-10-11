d56cd445f6d61c7d16db788602ecb16bb87b2a72
박현철 <hyunchul1998@naver.com>
2022-10-11 20:35:51 +0900

    go to vintage from home

--- frontend/hanium-client/src/routes/Home.js
+++ frontend/hanium-client/src/routes/Home.js
@@ -1,6 +1,9 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+    const navigate = useNavigate();
+    navigate('/vintages')
     return (
         <>
             메인화면입니다.
